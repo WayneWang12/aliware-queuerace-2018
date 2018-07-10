488f7a45001eec098587636915ecbf11098cc135
Wayne Wang <scweang@hotmail.com>
2018-07-11 05:47:55 +0800

    只能这样了。算了吧。

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public interface Constants {
     int msgBatch = 20;
-    long bufferSize = 1024;
+    long bufferSize = 1300;
     long blockSize = 64 * 1024;
     long MAX_DIRECT_BUFFER_SIZE = 3 * 1024 * 1024 * 1024l;
     String filePath = "/alidata1/race2018/data/rdp";
